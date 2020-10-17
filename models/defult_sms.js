31408ce8372c0d1ebe6fab08f5baf7d69133ce5e
unknown <lamirshahinxl@gmail.com>
2020-10-17 20:46:42 +0330

    all

---
+++ models/defult_sms.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const defultSms = new Schema({
+    sms: {
+        type: String, unique: false,
+    },
+    notif: {
+        type: String, unique: false,
+    },
+    link: {
+        type: String, unique: false,
+    }
+});
+module.exports = mongoose.model('defultSms', defultSms);
